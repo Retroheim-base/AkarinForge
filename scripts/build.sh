3fbfdbaece5abb105d00d701f28eef0c1d15b22a
retroheim <60027238+retroheim@users.noreply.github.com>
2020-02-21 21:07:18 -0500

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,13 +15,13 @@ if [ "$2" == "--setup" ] || [ "$3" == "--setup" ] || [ "$4" == "--setup" ]; then
 		./gradlew setupForge
 		
 		echo "[Akarin] Touch workspace.."
-		\cp -rf "$forgebasedir/projects/Forge/.settings" "$basedir/"
-		\cp -rf "$forgebasedir/projects/Forge/.project" "$basedir/"
-		\cp -rf "$forgebasedir/projects/Forge/.classpath" "$basedir/"
-		\cp -rf "$forgebasedir/projects/Forge/.settings" "$basedir/"
-		\cp -rf "$forgebasedir/projects/Forge/Forge.iml" "$basedir/"
-		\cp -rf "$forgebasedir/projects/Forge/Forge Client.launch" "$basedir/"
-		\cp -rf "$forgebasedir/projects/Forge/Forge Server.launch" "$basedir/"
+		#\cp -rf "$forgebasedir/projects/Forge/.settings" "$basedir/"
+		#\cp -rf "$forgebasedir/projects/Forge/.project" "$basedir/"
+		#\cp -rf "$forgebasedir/projects/Forge/.classpath" "$basedir/"
+		#\cp -rf "$forgebasedir/projects/Forge/.settings" "$basedir/"
+		#\cp -rf "$forgebasedir/projects/Forge/Forge.iml" "$basedir/"
+		#\cp -rf "$forgebasedir/projects/Forge/Forge Client.launch" "$basedir/"
+		#\cp -rf "$forgebasedir/projects/Forge/Forge Server.launch" "$basedir/"
 	)
 fi
 echo "[Akarin] Ready to build"
@@ -31,11 +31,11 @@ echo "[Akarin] Ready to build"
 	\cp -rf "$basedir/icon.ico" "$forgebasedir/"
 
 	echo "[Akarin] Touch forge.."
-	\rm -rf "$forgebasedir/projects/Forge/src"
+	#\rm -rf "$forgebasedir/projects/Forge/src"
 	\cp -rf "$basedir/sources/server/src" "$forgebasedir/projects/Forge/"
 	
 	echo "[Akarin] Touch server.."
-	\rm -rf "$forgebasedir/src"
+	#\rm -rf "$forgebasedir/src"
 	\cp -rf "$basedir/sources/forge/src" "$forgebasedir/"
 
 	cd "$forgebasedir"
@@ -44,7 +44,7 @@ echo "[Akarin] Ready to build"
 	echo "[Akarin] Brute Forcing"
 	\cp "$basedir/changelog_new.txt" "$forgebasedir/build/"
 	echo "[Akarin] Build Forge.. (2/2)"
-	./gradlew launch4j --debug
+	./gradlew launch4j
 
 	build="$forgebasedir/build/distributions"
 	\cp -rf "$build" "$basedir/"
